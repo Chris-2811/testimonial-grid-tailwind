d68410c189c033fdc2569ffd593ffb4b2f89acf4
Christoph Schmid <chris@MBPvonChristoph.fritz.box>
2023-08-28 18:23:09 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ['./*.html'],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px'
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+      colors: {
+        'moderate-violet': 'hsl(263, 55%, 52%)',
+        'very-dark-grayish-blue': 'hsl(217, 19%, 35%)',
+        'very-dark-blackish-blue': 'hsl(219, 29%, 14%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'light-gray': 'hsl(0, 0%, 81%)',
+        'light-grayish-blue': 'hsl(210, 46%, 95%)',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
